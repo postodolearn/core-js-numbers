a134e3a5a606b8071d8f14239e7877fd84d9be5b
Svyatoslav Kusnetsov <postodolearn@gmail.com>
2025-09-13 11:20:03 +0300

    12 passing

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -110,7 +110,10 @@ function getLinearEquationRoot(a, b) {
  */
 
 function getAngleBetweenVectors(x1, y1, x2, y2) {
-  return;
+  const point = x1 * x2 + y1 * y2;
+  const a = Math.sqrt(x1 * x1 + y1 * y1);
+  const b = Math.sqrt(x2 * x2 + y2 * y2);
+  return Math.acos(point / (a * b));
 }
 
 /**
@@ -126,8 +129,8 @@ function getAngleBetweenVectors(x1, y1, x2, y2) {
  *     5     => 5
  *     0     => 0
  */
-function getLastDigit(/* value */) {
-  throw new Error('Not implemented');
+function getLastDigit(value) {
+  return value % 10;
 }
 
 /**
@@ -141,8 +144,9 @@ function getLastDigit(/* value */) {
  *     '37'     => 37
  * '-525.5'     => -525.5
  */
-function parseNumberFromString(/* value */) {
-  throw new Error('Not implemented');
+
+function parseNumberFromString(value) {
+  return Number(value);
 }
 
 /**
@@ -158,8 +162,9 @@ function parseNumberFromString(/* value */) {
  *   3,3,3   => 5.196152422706632
  *   1,2,3   => 3.741657386773941
  */
-function getParallelepipedDiagonal(/* a, b, c */) {
-  throw new Error('Not implemented');
+
+function getParallelepipedDiagonal(a, b, c) {
+  return Math.sqrt(a * a + b * b + c * c);
 }
 
 /**
@@ -179,8 +184,10 @@ function getParallelepipedDiagonal(/* a, b, c */) {
  *   1678, 2  => 1700
  *   1678, 3  => 2000
  */
-function roundToPowerOfTen(/* num, pow */) {
-  throw new Error('Not implemented');
+
+function roundToPowerOfTen(num, pow) {
+  const decimal = 10 ** pow;
+  return Math.round(num / decimal) * decimal;
 }
 
 /**
@@ -200,8 +207,15 @@ function roundToPowerOfTen(/* num, pow */) {
  *   16 => false
  *   17 => true
  */
-function isPrime(/* n */) {
-  throw new Error('Not implemented');
+
+function isPrime(n) {
+  if (n === 2) return true;
+  if (n <= 1 || n % 2 === 0) return false;
+
+  for (let i = 3; i <= Math.sqrt(n); i += 2) {
+    if (n % i === 0) return false;
+  }
+  return true;
 }
 
 /**
